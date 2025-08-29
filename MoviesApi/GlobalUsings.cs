2c814ce6117c73d773143b6e450927e4f17848ab
Youssef-Darrag <darrag904@gmail.com>
2025-08-29 22:44:18 +0300

    Add Genres model.

---
+++ MoviesApi/GlobalUsings.cs
@@ -0,0 +1,3 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using MoviesApi.Models;
+global using System.ComponentModel.DataAnnotations;
--- MoviesApi/Models/ApplicationDbContext.cs
+++ MoviesApi/Models/ApplicationDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace MoviesApi.Models
+﻿namespace MoviesApi.Models
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        public DbSet<Genre> Genres { get; set; }
     }
 }
